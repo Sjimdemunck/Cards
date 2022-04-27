869b9f996a0ed68574845801962543b9a6deb796
Sjim de Munck <s.munck@hoffelijk.nl>
2022-04-27 22:30:26 +0200

    Play around with firebase / auth

--- src/containers/Authentication.tsx
+++ src/containers/Authentication.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { Form, Button, Card} from 'react-bootstrap'
 
 export interface IUser {
     id: string,
@@ -13,4 +14,40 @@ export interface IPerson {
     age: number,
     gender: string,
     Avatar: string,
+}
+
+const Authentication = () => {
+    const emailRef = useRef<any>();
+    const passwordRef = useRef<any>();
+    const passwordConfirmRef = useRef<any>();
+
+    return (
+        <>
+        <Card>
+            <Card.Body>
+                <h2 className="text-center mb-4"> Sign Up</h2>
+                <Form>
+                    <Form.Group id="email">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" ref={emailRef} required/>
+                    </Form.Group>
+                    <Form.Group id="password">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" ref={passwordRef} required/>
+                    </Form.Group>
+                    <Form.Group id="password-confirm">
+                        <Form.Label>Password Confirmation</Form.Label>
+                        <Form.Control type="password" ref={passwordConfirmRef} required/>
+                    </Form.Group>
+                    <Button className="w-100" type="submit">
+                        Sign up
+                    </Button>
+                </Form>
+            </Card.Body>
+        </Card>
+            <div className="w-100 text-center mt-2"></div>
+            Already have an account? Log in
+        </>
+    )
+
 }
--- src/containers/Paper.tsx
+++ src/containers/Paper.tsx
@@ -15,9 +15,7 @@ export default function SimplePaper() {
                 },
             }}
         >
-            <Paper elevation={0} />
             <Paper />
-            <Paper elevation={3} />
         </Box>
     );
 }
